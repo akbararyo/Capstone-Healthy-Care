9b95a95a6e6f92362561de665748b6e98c3ee8bb
akbararyo <akbararyo@gmail.com>
2021-12-09 18:46:53 +0700

    adding project

---
+++ app/src/main/java/com/example/capstone_healthycare/FoodRecipe.kt
@@ -0,0 +1,11 @@
+package com.example.capstone_healthycare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodRecipe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_recipe)
+    }
+}
---
+++ app/src/main/java/com/example/capstone_healthycare/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.capstone_healthycare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.capstone_healthycare.databinding.ActivityMainBinding
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+
+
+    }
+
+}
